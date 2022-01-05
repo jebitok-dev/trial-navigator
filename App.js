15d21bff7494929f87784cd7420807c178b2bc1b
jebitok-dev <sharonkosgei4@gmail.com>
2022-01-05 16:14:06 +0300

    Add Changes to App

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import {View, Text} from "react-native";
+import { NavigationContainer } from "@react-native";
+import {createNativeStackNavigator} from "@react-navigation/native"
+import HomeScreen from "./src/HomeScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
